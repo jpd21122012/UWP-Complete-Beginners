c683464bd71f4f039866f8cee6c1d2b3f7261a99
Ing. Jorge Perales Diaz <slipknot_jpd@hotmail.com>
2018-01-22 13:12:30 -0600

    Complete Course

---
+++ UWP-031/UWPWeather/UPWWeatherService/Controllers/HomeViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UPWWeatherService.Controllers
+{
+    public class HomeViewController : Controller
+    {
+        // GET: HomeView
+        public async Task<ActionResult> Index(string lat, string lon)
+        {
+            var latitude = double.Parse(lat.Substring(0, 5));
+            var longitude = double.Parse(lon.Substring(0, 5));
+            var weather = await Models.OpenWeatherMapProxy.GetWeather(latitude, longitude);
+            ViewBag.Name = weather.name;
+            ViewBag.Temp = ((int)weather.main.temp).ToString();
+            ViewBag.Description = weather.weather[0].description;
+
+            return View();
+        }
+    }
+}
---
+++ UWP-031/UWPWeather/UPWWeatherService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UPWWeatherService.MvcApplication" Language="C#" %>
